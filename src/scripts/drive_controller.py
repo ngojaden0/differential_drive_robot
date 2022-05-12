0df440adc650a5df8d3ef2e1efdd133a34c6b826
ngojaden0 <ngojaden0@github.com>
2022-05-12 14:11:45 -0500

    completed kinematic model

--- src/scripts/drive_controller.py
+++ src/scripts/drive_controller.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+from cmath import pi
 from requests import NullHandler
 import rospy
 from math import sin
@@ -8,13 +9,15 @@ from nav_msgs.msg import Odometry
 import tf
 from sensor_msgs.msg import JointState
 import time
-length = 0.1884 #distance between midpoint of each wheel
+import numpy as np
+length = 0.1883 #distance between midpoint of each wheel
 radius = 0.0381
 prev_rad_R = 0.0
 prev_rad_L = 0.0
-currT = rospy.Time()
-
-#callback for actual speed (real robo)
+th = 0.0
+x = 0.0
+y = 0.0
+#callback for actual speed (real robot)
 def callback(speed_msg): 
     rospy.loginfo("right: "+str(speed_msg.vector.x)+"  left: "+str(speed_msg.vector.y)+"--")
 
@@ -25,20 +28,25 @@ def feedback(simulation_speed):
     rad_L = simulation_speed.position[1]
     global prev_rad_R
     global prev_rad_L
-    global currT
+    global th, x, y
     vel_R  = ((rad_R - prev_rad_R)/0.1)*radius
     vel_L  = ((rad_L - prev_rad_L)/0.1)*radius
-    if vel_R == vel_L:
-        rotation = 0
-        R = float('inf')
-    else:
-        R = (length/2)*(vel_R+vel_L)/(vel_R-vel_L) #instantaneous center of curvature to mid of robot
-        rotation = (vel_R-vel_L)/length #robot angular velocity around ICC
-        #ICC = [x-R*sin(theta), y-R*cos(theta)]  
-    print(" "+str(vel_R)+" "+str(vel_L)+"   "+str(currT.to_sec()))    
+    vel_avg = (vel_R + vel_L)/2
+    dth = ((vel_R - vel_L)*0.1)/length
+    dxy = vel_avg*0.1
+    dx = dxy*cos(dth)
+    dy = dxy*sin(dth)
+    x += (cos(th)*dx-sin(th)*dy)
+    y += (sin(th)*dx+cos(th)*dy)
+    th += dth
+    print(" "+str(vel_R)+" "+str(vel_L)+"   ")
+    print("\n"+str(x)+" "+str(y)+" "+str(th)) 
     prev_rad_R = rad_R
     prev_rad_L = rad_L
-
+    if th >= 2*pi: 
+        th -= 2*pi
+    if th <= -2*pi: 
+        th += 2*pi
 
 #callback for pose/twist (gazebo)
 def getback(odom):
