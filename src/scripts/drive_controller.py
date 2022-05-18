165f1dfa3eaca557a042df6d495f76ceb96ff2e6
ngojaden0 <ngojaden0@github.com>
2022-05-17 23:22:11 -0500

    updates

--- src/scripts/drive_controller.py
+++ src/scripts/drive_controller.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 from cmath import pi
 from requests import NullHandler
 import rospy
@@ -10,6 +10,7 @@ import tf
 from sensor_msgs.msg import JointState
 import time
 import numpy as np
+
 length = 0.1883 #distance between midpoint of each wheel
 radius = 0.0381
 prev_rad_R = 0.0
@@ -17,15 +18,16 @@ prev_rad_L = 0.0
 th = 0.0
 x = 0.0
 y = 0.0
+
 #callback for actual speed (real robot)
 def callback(speed_msg): 
     rospy.loginfo("right: "+str(speed_msg.vector.x)+"  left: "+str(speed_msg.vector.y)+"--")
 
 #callback for joint states (gazebo)
 def feedback(simulation_speed):
+    global prev_rad_R, prev_rad_L, th, x, y
     rad_R = simulation_speed.position[0]
     rad_L = simulation_speed.position[1]
-    global prev_rad_R, prev_rad_L, th, x, y
     vel_R  = ((rad_R - prev_rad_R)/0.1)*radius
     vel_L  = ((rad_L - prev_rad_L)/0.1)*radius
     vel_avg = (vel_R + vel_L)/2
@@ -46,6 +48,9 @@ def feedback(simulation_speed):
     if th <= -2*pi: 
         th += 2*pi
 
+    br = tf.TransformBroadcaster()
+    br.sendTransform((x,y,0),tf.transformations.quarternion_from_euler(0,0,th),rospy.Time.now(),"robot","odometry")
+
     rospy.loginfo("\nvelocity:\n"+"  right_wheel (m/s):"+
         str(vel_R)+"\n  left_wheel (m/s):"+
         str(vel_L)+"\n  total_direction (m/s):"+
@@ -53,17 +58,11 @@ def feedback(simulation_speed):
         str(angular_vel)+"\npose:\n  x (m):"+
         str(x)+"\n  y (m):"+str(y)+"\n  theta (rad):"+str(th)+"\n")
 
-#callback for pose/twist (gazebo)
-def getback(odom):
-    rospy.loginfo("\nodom:\n  position:\n    x:"+str(odom.pose.pose.position.x)+
-    "\n"+"    y:"+str(odom.pose.pose.position.y)+"\n")
-    
 if __name__ == '__main__':
     try:            
         rospy.init_node('drive_controller')  
         #rospy.Subscriber('speed', Vector3Stamped, callback)
         rospy.Subscriber('joint_states', JointState, feedback)
-        #rospy.Subscriber('odom', Odometry, getback)
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             rospy.spin()
